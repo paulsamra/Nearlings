76053007f3c996760583d3bb20862ff7de9cd397
edbert <edbert.chan@yahoo.com>
2014-09-29 15:38:44 -0700

    Initial commit.

---
+++ src/com/edbert/library/navigationdrawer/NavDrawerItemInterface.java
@@ -0,0 +1,23 @@
+package com.edbert.library.navigationdrawer;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.view.MenuItem;
+
+public interface NavDrawerItemInterface {
+	public enum Type {
+		LOGOUT_TYPE, SECTION_TYPE, ITEM_TYPE
+		}
+	
+	public void launchActivity(Context c);
+
+	public Fragment getFragment();
+
+	public boolean updateActionBarTitle();
+	
+	public String actionBarTitle();
+
+
+}
